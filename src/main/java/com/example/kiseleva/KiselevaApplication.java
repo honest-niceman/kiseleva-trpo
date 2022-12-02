ffc480dc116bd875da1cea47d1f0b7ad3add6ec0
Georgii Vlasov <honest.niceman@gmail.com>
2022-12-02 15:51:57 +0400

    Init commit

---
+++ src/main/java/com/example/kiseleva/KiselevaApplication.java
@@ -0,0 +1,13 @@
+package com.example.kiseleva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiselevaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiselevaApplication.class, args);
+    }
+
+}
